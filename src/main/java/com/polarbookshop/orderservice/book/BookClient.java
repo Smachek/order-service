49eedc6069998ada4552f65f38e9a679d430624f
Segey Smachek <sergeysmachek@gmail.com>
2023-03-27 01:38:52 +0300

    Defining retries with wxponential backoff for HTTP call Catalog Service.

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -4,6 +4,7 @@ import com.polarbookshop.orderservice.config.ClientProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 import java.time.Duration;
 
@@ -24,6 +25,7 @@ public class BookClient {
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()
                 .bodyToMono(Book.class)
-                .timeout(Duration.ofSeconds(clientProperties.catalogServiceTimeout()), Mono.empty());
+                .timeout(Duration.ofSeconds(clientProperties.catalogServiceTimeout()), Mono.empty())
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)));
     }
 }
